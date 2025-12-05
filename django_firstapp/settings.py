2ae2779abf696f68be7e78536ce910071f0e710e
Ayan <ayanguradi2@gmail.com>
2025-12-05 13:15:53 +0530

    static changes done

--- django_firstapp/settings.py
+++ django_firstapp/settings.py
@@ -129,8 +129,7 @@ STATIC_URL = '/static/'
 # This directory MUST be outside of your project directory for production best practices
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
